61f64ba410914038e63fa99ee164fee8fbae53be
adimancifi <46333102+adimancifi@users.noreply.github.com>
2021-09-10 07:47:39 +0800

    merge repository

---
+++ app/logs/log-2021-09-09.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-09-09 15:55:37 --> 404 Page Not Found: L-content/favicon
+ERROR - 2021-09-09 15:57:17 --> 404 Page Not Found: L-content/favicon
